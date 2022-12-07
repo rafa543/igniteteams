1d91ed1472fc12c2e25bb8d8d645c237a8d8bcc5
Rafael Araujo <rafa123839@gmail.com>
2022-12-07 18:39:52 -0300

    terminado tela de turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,35 @@
+import { Button } from '@components/Button';
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/HighLight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
+      <Header />
+      <Highlight
+        title='Turmas'
+        subtitle='joque com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma" />}
+      />
+      <Button title='Criar nova turma' />
     </Container>
   );
 }
